127cf586a809f0ebf5e87aa4d80d53404c80c8ee
Danyy41 <dany.mhaidly@gmail.com>
2026-06-07 23:45:30 +0000

    Add factual critic node to LangGraph

--- langgraph_workflow.py
+++ langgraph_workflow.py
@@ -1,13 +1,15 @@
 from typing import TypedDict
 from langgraph.graph import StateGraph, START, END
 
+from agents.generator import generate_answer
+from agents.factual_critic import check_facts
+
 
 class WorkflowState(TypedDict):
     question: str
     answer: str
+    factual_critique: dict
 
-
-from agents.generator import generate_answer
 
 def generator_node(state):
     answer = generate_answer(state["question"])
@@ -16,12 +18,21 @@ def generator_node(state):
     }
 
 
+def factual_critic_node(state):
+    critique = check_facts(state["answer"])
+    return {
+        "factual_critique": critique
+    }
+
+
 graph = StateGraph(WorkflowState)
 
 graph.add_node("generator", generator_node)
+graph.add_node("factual_critic", factual_critic_node)
 
 graph.add_edge(START, "generator")
-graph.add_edge("generator", END)
+graph.add_edge("generator", "factual_critic")
+graph.add_edge("factual_critic", END)
 
 app = graph.compile()
 
